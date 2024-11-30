764cbd51dbfddb298c4d030d25694a8cc6b4ccf3
dxse <dm739165@gmail.com>
2024-12-01 01:51:00 +0300

    made a jobs and an individual job dinamic pages

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:title>
-        About page
-    </x-slot:title>    
-    <h1>About page</h1>
-</x-layout>
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -2,5 +2,5 @@
     <x-slot:title>
         Contacts page
     </x-slot:title>
-    <h1>Contact page</h1>
+    <h1 class="font-bold text-lg">Contact page</h1>
 </x-layout>
